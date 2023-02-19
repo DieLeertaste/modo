037ef62d8de63dd73831f0b664712d2001a4ae5b
Noah <noah.p.kleineweischede@gmail.com>
2023-02-19 21:13:06 +0100

    Commit

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,15 +3,19 @@ const express = require('express')
 
 // Create Dashboard
 const app = express()
+app.enable('trust proxy') // if the ip is ::1 it means localhost
+app.set('etag', false) // disable chache
+app.use(express.static(__dirname + '/dashboard'))
+
+app.use((req, res, next) => {
+    console.log(`- ${req.method}: ${req.url} ${res.statusCode} (by: ${req.ip})`)
+    next()
+})
+
+app.get('/', async (req, res) => {
+    res.sendFile('src/dashboard/html/home.html')
+})
 
 // Start Dashboard
 app.listen(3000)
 
-// Routes
-app.get('/', function(req, res) {
-    res.send('Hello World')
-})
-
-app.get('/lol', function(req, res) {
-    res.send('lol')
-})
